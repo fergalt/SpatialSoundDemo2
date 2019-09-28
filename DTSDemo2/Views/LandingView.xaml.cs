428f984cccdfdfca8f910a01d124c9fc878c5c59
Fergal Toohey <29102967+fergalt@users.noreply.github.com>
2019-09-28 23:42:49 +0100

    Adding position tracking behavior

--- DTSDemo2/Views/LandingView.xaml.cs
+++ DTSDemo2/Views/LandingView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,88 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DTSDemo2
+namespace DTSDemo2.Views
 {
     /// <summary>
     /// Interaction logic for LandingView.xaml
     /// </summary>
-    public partial class LandingView : Page
+    public partial class LandingView : UserControl, INotifyPropertyChanged
     {
-        public string YPos = "hello";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double x;
+        public double y;
+        public double z;
+
         public LandingView()
         {
             InitializeComponent();
+            this.DataContext = this;
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+                OnPropertyChanged("X");
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+                OnPropertyChanged("Y");
+            }
+        }
+
+        public double Z
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;
+                OnPropertyChanged("Z");
+            }
+        }
+
+
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void MouseDragElementBehavior_Dragging(object sender, MouseEventArgs e)
+        {
+            Point PxPosition = Emitter.TranslatePoint(new Point(Emitter.Width / 2 - Listener.Width / 2, Emitter.Height / 2 - Listener.Height / 2), Listener);
+            Point MetricPosition = new Point(PxPosition.X / Canvas.Width, PxPosition.Y / Canvas.Height);
+            X = MetricPosition.X;
+            Y = MetricPosition.Y;
         }
     }
+
 }
